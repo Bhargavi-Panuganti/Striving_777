56553aaa2e33d9a81b2fc125a90568c42a32446f
Bhargavi <161690421+Bhargavi-Panuganti@users.noreply.github.com>
2026-05-24 21:25:11 +0530

    Time: 12 ms (47.4%), Space: 56.5 MB (47.82%) - LeetHub

---
+++ 0518-coin-change-ii/0518-coin-change-ii.java
@@ -0,0 +1,28 @@
+class Solution {
+    public int change(int amount, int[] coins) {
+        int n = coins.length;
+        int[][] dp = new int[n + 1][amount + 1];
+    
+        for (int i = 0; i <= n; i++)
+            dp[i][0] = 1;
+
+        return solve(coins, amount, dp);
+    }
+
+     public int solve(int[] coins, int amount, int[][] dp) {
+        for (int i = 1; i <= coins.length; i++) {
+            for (int j = 1; j <= amount; j++) {
+                if (coins[i - 1] <= j) {
+                    int take = dp[i][j - coins[i - 1]];
+                    if (take != Integer.MAX_VALUE) // ✅ overflow check
+                        dp[i][j] = dp[i - 1][j]+ take;
+                    else
+                        dp[i][j] = dp[i - 1][j];
+                } else {
+                    dp[i][j] = dp[i - 1][j];
+                }
+            }
+        }
+        return dp[coins.length][amount];
+    }
+}
