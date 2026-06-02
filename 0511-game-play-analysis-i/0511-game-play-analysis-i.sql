e4e1e61fe2175d235fa35b134deaf8731a9613f4
Bhargavi <161690421+Bhargavi-Panuganti@users.noreply.github.com>
2026-06-03 05:03:27 +0530

    Time: 503 ms (50.4%), Space: 0B (100%) - LeetHub

---
+++ 0511-game-play-analysis-i/0511-game-play-analysis-i.sql
@@ -0,0 +1,9 @@
+# Write your MySQL query statement below
+select player_id, event_date as first_login
+from (
+   select player_id, event_date,dense_rank() over(
+    partition by player_id 
+    order by event_date asc) as rnk
+   from Activity
+)t
+where rnk=1;
